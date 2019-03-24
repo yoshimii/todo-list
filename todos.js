c72da6114f1bda2c1021c1c4d48125d9ec97c521
Emma Zarate <zarate.emm@gmail.com>
2019-03-24 13:12:58 +0800

    Add files via upload

---
+++ todos.js
@@ -0,0 +1,26 @@
+//Check off specific todos by clicking
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //grab new todo from input
+        var todoText = $(this).val();
+        //clearing text from input
+        $(this).val("");
+        //create a new li and add ul
+        $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>")    
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+});
